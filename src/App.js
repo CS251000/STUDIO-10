3edbef0cc9a9d5a79b195974d84230c56d20f171
chirag <chiragsinghal04@gmail.com>
2024-06-28 00:49:00 +0530

    -protect

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import CardDetails from "./components/CardDetails";
 import Reordered from "./components/Reordered";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-
+import ProtectedRoute from './components/ProtectedRouter';
+import { AuthProvider } from "./hooks/useAuth";
 
 const Layout = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -25,21 +26,20 @@ const router = createBrowserRouter([
     path: "/",
     element: <Layout />,
     children: [
-      { index: true, element: <Home/> },
-      { path: "/:itemId", element: <CardDetails /> },
-      { path: "add", element: <AddItem /> },
-      { path: "reordered", element: <Reordered /> },
+      { index: true, element: <ProtectedRoute><Home /></ProtectedRoute> },
+      { path: "/:itemId", element: <ProtectedRoute><CardDetails /></ProtectedRoute> },
+      { path: "add", element: <ProtectedRoute><AddItem /></ProtectedRoute> },
+      { path: "reordered", element: <ProtectedRoute><Reordered /></ProtectedRoute> },
       { path: "login", element: <Login /> },
       { path: "signup", element: <Signup /> }
-
     ]
   }
 ]);
 
 export default function App() {
   return (
-    <>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </>
+    </AuthProvider>
   );
 }
